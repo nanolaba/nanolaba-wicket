693fd405de0aeb467f5aba45a4c81ab5d6a44acb
alexander.andriishin <alexander@andriishin.com>
2016-03-11 18:09:48 +0300

    0.0.1

---
+++ src/main/java/com/nanolaba/wicket/components/FSubmitLink.java
@@ -0,0 +1,39 @@
+package com.nanolaba.wicket.components;
+
+import com.nanolaba.wicket.interfaces.Action;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.SubmitLink;
+import org.apache.wicket.model.IModel;
+
+
+public class FSubmitLink extends SubmitLink {
+
+    private static final long serialVersionUID = 6599469691030810179L;
+
+    private final Action action;
+
+    public FSubmitLink(String id, Action action) {
+        super(id);
+        this.action = action;
+    }
+
+    public FSubmitLink(String id, Form<?> form, Action action) {
+        super(id, form);
+        this.action = action;
+    }
+
+    public FSubmitLink(String id, IModel<?> model, Action action) {
+        super(id, model);
+        this.action = action;
+    }
+
+    public FSubmitLink(String id, IModel<?> model, Form<?> form, Action action) {
+        super(id, model, form);
+        this.action = action;
+    }
+
+    @Override
+    public void onSubmit() {
+        action.makeAction();
+    }
+}
