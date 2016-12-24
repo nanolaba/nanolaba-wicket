b370f703463d8f9ee7119ce604e7e87589719d79
alexander.andriishin <alexander@andriishin.com>
2016-12-24 21:33:55 +0300

    0.0.16

--- src/main/java/com/nanolaba/wicket/util/IWicketUtils.java
+++ src/main/java/com/nanolaba/wicket/util/IWicketUtils.java
@@ -5,17 +5,17 @@ import org.apache.wicket.request.cycle.RequestCycle;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public interface WicketUtils {
+public interface IWicketUtils {
 
-    static HttpServletResponse getHttpServletResponse() {
+    default HttpServletResponse getHttpServletResponse() {
         return (HttpServletResponse) RequestCycle.get().getResponse().getContainerResponse();
     }
 
-    static HttpServletRequest getHttpServletRequest() {
+    default HttpServletRequest getHttpServletRequest() {
         return (HttpServletRequest) RequestCycle.get().getRequest().getContainerRequest();
     }
 
-    static void sendPermanentRedirect(String url) {
+    default void sendPermanentRedirect(String url) {
         HttpServletResponse response = getHttpServletResponse();
         response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
         response.setHeader("Location", url);
